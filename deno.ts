d0be213a6fb546114e4729041ad95931617332f5
cherylh111 <89995150+cherylh111@users.noreply.github.com>
2023-06-10 14:37:27 +0800

    Update deno.ts
    debug

--- deno.ts
+++ deno.ts
@@ -3,6 +3,8 @@ import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 const OPENAI_API_HOST = "chat.openai.com";
 const PANDORA_API_PATH = "api/conversations";
 const CHAT_API_PATH = "backend-api/conversations";
+const PANDORA_API_PATH1 = "api/conversation";
+const CHAT_API_PATH1 = "backend-api/conversation";
 
 serve(async (request) => {
   const url = new URL(request.url);
@@ -13,11 +15,20 @@ serve(async (request) => {
   
   url.host = OPENAI_API_HOST;
   
-  if (url.pathname.includes("api/conversations")) {
+  if (url.pathname.includes(PANDORA_API_PATH)) {
     const newUrl = url.replace(PANDORA_API_PATH, CHAT_API_PATH);
+    console.log("URL:", url);
+    console.log("Request:", request);
     return await fetch(newUrl, request);
   }
 
+ if (url.pathname.includes(PANDORA_API_PATH1)) {
+    const newUrl = url.replace(PANDORA_API_PATH1, CHAT_API_PATH1);
+    console.log("URL:", url);
+    console.log("Request:", request);
+    return await fetch(newUrl, request);
+  }
+  
   console.log("URL:", url);
   console.log("Request:", request);
   return await fetch(url, request);
