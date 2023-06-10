89b6983f34a0b42898dba92405ee560951d86a3c
cherylh111 <89995150+cherylh111@users.noreply.github.com>
2023-06-10 14:23:55 +0800

    Update deno.ts
    测试

--- deno.ts
+++ deno.ts
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
-const OPENAI_API_HOST = "api.openai.com";
+const OPENAI_API_HOST = "chat.openai.com";
+const PANDORA_API_PATH = "api/conversations";
+const CHAT_API_PATH = "backend-api/conversations";
 
 serve(async (request) => {
   const url = new URL(request.url);
@@ -8,8 +10,14 @@ serve(async (request) => {
   if (url.pathname === "/") {
     return fetch(new URL("./Readme.md", import.meta.url));
   }
-
+  
   url.host = OPENAI_API_HOST;
+  
+  if (url.pathname.includes("api/conversations")) {
+    const newUrl = url.replace(PANDORA_API_PATH, CHAT_API_PATH);
+    return await fetch(url, request);
+  }
+
   console.log("URL:", url);
   console.log("Request:", request);
   return await fetch(url, request);
